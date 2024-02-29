7099ce7bbc46ec363b5ebff48b0d13f444a5e504
Steggy <steggy@steg.gy>
2024-02-28 23:34:20 -0500

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include "raylib.h"
+
+int main(void)
+{
+	InitWindow(800, 600, "Cross-platform raylib template");
+
+	SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+	SetTargetFPS(60);
+
+	// Image raylibImg = LoadImage("../res/raylib.png");
+	Texture2D raylibTex = LoadTexture("../res/raylib.png");
+
+	while(!WindowShouldClose()) {
+		int width = GetRenderWidth();
+		int height = GetRenderHeight();
+
+BeginDrawing();
+
+	ClearBackground(RAYWHITE);
+
+	DrawTexture(raylibTex, (width - raylibTex.width) / 2, (height - raylibTex.height) / 2, WHITE);
+
+	DrawFPS(0, 0);
+
+EndDrawing();
+
+	}
+
+	CloseWindow();
+
+	return 0;
+}
